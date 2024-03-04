f7fe3c9cede078bcc5afb293365e7ef2227e378c
adriangd03 <80117534+adrigd03@users.noreply.github.com>
2024-03-04 20:22:38 +0100

    Oauth i creació migració usuaris
    Acabant el inici de sessió amb Oauth. ambé he creat la migració de usuaris però encara no he afegit res. També he afegit la llibreria de Socialite ab composer.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Usuari;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+})->middleware('guest')->name('login');
+
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
 });
+
+Route::get('/google-callback', function (Request $request) {
+
+    try {
+        $user = Socialite::driver('google')->user();
+    } catch (\Exception $e) {
+        return redirect()->route('/')->withErrors('Error al obtenir les dades del usuari.');
+    }
+
+
+    // //verificar si el usuario existe en la base de datos y si no existe redirigir a la ruta "/" con un mensaje de error y si existe loguearlo
+    // $userDB = Usuari::where('email', $user->email)->first();
+
+    // if (!$userDB) {
+    //     return redirect()->route('/')->withErrors('No tens permís per accedir a aquesta aplicació.');
+    // }
+
+
+    // Auth::login($userDB);
+
+    // $request->session()->put('avatarUrl', $user->getAvatar());
+
+    // redireccionar al home
+    return redirect()->route('home');
+});
+
+Route::get('/home', function (Request $request) {
+    return view('home');
+})->middleware('auth')->name('home');
